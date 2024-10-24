06b22578f034b64e9d32e136a9a6f7d6dc0d238a
Reihan Agam <reihanagam7@gmail.com>
2024-10-24 19:16:55 +0700

    update

--- app/Views/home/index.php
+++ app/Views/home/index.php
@@ -2,43 +2,30 @@
 
 <?= $this->section('content') ?>
 
-<div class="flex justify-center items-center">
-    <div class="rounded-md mx-10">
-        <img src="<?= base_url('public/assets/image/banner.png') ?>?t=" . time()" class="object-fit" alt="banner" width="1300" />
+<div class="flex flex-col md:flex-row justify-center items-center">
+    <div class="mx-10">
+        <img src="<?= base_url('public/assets/image/banner-new.png') ?>?t=" . time()" style="width: 600px;" alt="banner-1"/>
+        <div class="flex flex-col justify-start items-start mt-8 ml-4 pb-12">
+            <a href="https://info.tentangloker.com/" class="flex items-center btn-register">
+                <img src="<?= base_url('public/assets/image/Cs-icon.png') ?>" class="object-fit image-icon" alt="info-loker" />
+                <h2 class="text-white font-bold">Lewat Chat Bot Kami!</h2>
+            </a>
+        </div>
+    </div>
+    <div class="mx-10">
+        <img src="<?= base_url('public/assets/image/people.png')?>" style="width: 600px;" alt="banner-2">
     </div>
 </div>
 
-<div class="flex flex-col md:flex-col justify-center items-center mt-8 pb-12">
-    <!-- <div class="my-10 mx-10">
-            <a href="https://api.whatsapp.com/send?phone=+6285212131890&text=Loker"> <img src="<?= base_url() ?>assets/image/info-loker.png" class="object-fit info-loker-c" alt="info-loker" /> </a>
-        </div> -->
-    <!-- <div class="my-4 mx-10">
-        <a href="https://api.whatsapp.com/send?phone=+6285212131890&text=Loker" class="flex items-center btn-wa">
-            <img src="<?= base_url('public/assets/image/WhatsApp.png') ?>" class="object-fit image-icon-wa" alt="info-loker" />
-            <h2 class="text-bot-wa">Info Loker Via Whatsapp</h2>
-        </a>
-    </div> -->
+<!-- <div class="flex flex-col md:flex-col justify-center items-center mt-8 pb-12">
     <div class="w-96 flex items-center justify-center">
-        <!-- <hr class="w-full bg-gray-400 line-hr"> -->
-        <p class="text-base font-medium leading-4 px-2.5 text-atau">Hubungi</p>
-        <!-- <hr class="w-full bg-gray-400 line-hr"> -->
     </div>
-    <!-- <div class="flex justify-start items-center my-10 mx-10">
-            <a href="#" class="flex items-center text-bot">
-                <div class=" btn-register">
-                    <img src="<?= base_url() ?>assets/image/Cs-icon.png" class="object-fit image-icon" alt="info-loker" />
-                    <h2>Lewat Chat Bot Kami!</h2>
-                </div>
-            </a>
-        </div> -->
     <div class="my-4 mx-10">
         <a href="https://info.tentangloker.com/" class="flex items-center btn-register">
             <img src="<?= base_url('public/assets/image/Cs-icon.png') ?>" class="object-fit image-icon" alt="info-loker" />
             <h2 class="text-bot">Lewat Chat Bot Kami!</h2>
         </a>
     </div>
-</div>
-
-
+</div> -->
 
 <?= $this->endSection() ?>
--- app/Views/layout/layouts.php
+++ app/Views/layout/layouts.php
@@ -7,7 +7,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>CariKerjaYuk</title>
-    <link rel="icon" type="image/x-icon" href="<?= base_url('public/assets/image/logo.png') ?>" />
+    <link rel="icon" type="image/x-icon" href="<?= base_url('public/assets/image/logo.png') ?>?t=" . time()" />
     <meta name="description" content="Penyedia lowongan kerja yang memudahkan pelamar mencari kerja tanpa ribet">
     <meta name="keywords" content="Cari Kerja Yuk, cari, kerja, yuk, cari">
 
@@ -15,7 +15,7 @@
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
 
     <!-- add custome css -->
-    <link href="<?= base_url("public/assets/css/custom.css") ?>" rel="stylesheet" type="text/css" />
+    <link href="<?= base_url("public/assets/css/custom.css") ?>?t=". time() rel="stylesheet" type="text/css" />
 
     <!-- popup sweetalert -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11.7.0/dist/sweetalert2.min.css">
@@ -23,29 +23,38 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css" />
 </head>
 
-<body class="flex flex-col">
+<body class="flex flex-col" style="background-color: #FBB94D;">
 
-    <nav class="flex justify-between items-center">
+    <nav class="flex justify-end items-center mr-60">
 
         <!-- LOGO -->
-        <div class="flex py-4 my-4 mx-12">
+        <!-- <div class="flex py-4 my-4 mx-12">
             <a href="<?= base_url() ?>">
-                <img src="<?= base_url('public/assets/image/logo.png') ?>?t=" . time()" alt="Logo" class="logo-c">
+                <img src="<?= base_url('public/assets/image/logo.png') ?>?t=" . time() alt="Logo" class="logo-c">
             </a>
-        </div>
+        </div> -->
 
         <!-- NAVIGATION -->
-        <div class="hidden lg:block navbar-c flex-1 rounded-md py-4 my-4 ml-2 mr-12" style="background-color: #F99A1C;">
-            <div class="container mx-auto flex justify-around items-center">
-                <a href="<?= base_url() ?>" class="<?=$uri->getSegment(1) === '' ? ' font-bold text-white' : 'text-white ' ?>">Home</a>
-                <a href="<?= base_url() ?>/term-and-condition" class="<?=$uri->getSegment(1) === 'term-and-condition' ? ' font-bold text-white' : 'text-white' ?>">Term & Condition</a>
-                <a href="<?= base_url() ?>/about-us" class="<?=$uri->getSegment(1) === 'about-us' ? ' font-bold text-white' : 'text-white' ?>">About Us</a>
-                <!-- <a href="<?= base_url() ?>/how-to-use" class="<?=$uri->getSegment(1) === 'how-to-use' ? ' font-bold text-white' : 'text-white' ?>">How To Use</a> -->
+        <div class="hidden lg:block navbar-c rounded-md py-4 my-4">
+            <div class="container-menu">
+                <div class="box menu-box">
+                    <a href="<?= base_url() ?>" class="text <?=$uri->getSegment(1) === '' ? ' font-bold text-white' : 'text-white ' ?>">Home</a>
+                </div>
+                <div class="box menu-box">
+                    <a href="<?= base_url() ?>/term-and-condition" class="text <?=$uri->getSegment(1) === 'term-and-condition' ? ' font-bold text-white' : 'text-white' ?>">Term & Condition</a>
+                </div>
+                <div class="box menu-box">
+                    <a href="<?= base_url() ?>/about-us" class="text <?=$uri->getSegment(1) === 'about-us' ? ' font-bold text-white' : 'text-white' ?>">About Us</a>
+                </div>
             </div>
+            <!-- <div class="container mx-auto flex justify-around items-center">
+                <a href="<?= base_url() ?>/term-and-condition" class="<?=$uri->getSegment(1) === 'term-and-condition' ? ' font-bold text-white' : 'text-white' ?>">Term & Condition</a>
+                <a href="<?= base_url() ?>/about-us" class="<?=$uri->getSegment(1) === 'about-us' ? ' font-bold text-white' : 'text-white' ?>">About Us</a> -->
+                <!-- <a href="<?= base_url() ?>/how-to-use" class="<?=$uri->getSegment(1) === 'how-to-use' ? ' font-bold text-white' : 'text-white' ?>">How To Use</a> -->
+            <!-- </div> -->
         </div>
-
         <!-- Mobile Menu Button (Hidden on larger screens) -->
-        <div class="lg:hidden navbar-c rounded-md my-4 mr-12">
+        <div class="lg:hidden navbar-c rounded-md my-4 py-4 mr-12">
             <div class="menu-toggle">
                 <span></span>
                 <span></span>
@@ -56,7 +65,6 @@
                     <li><a href="<?= base_url() ?>" class="text-sm">Home</a></li>
                     <li><a href="<?= base_url() ?>/term-and-condition" class="text-sm">Term & Condition</a></li>
                     <li><a href="<?= base_url() ?>/about-us" class="text-sm">About Us</a></li>
-                    <!-- <li><a href="<?= base_url() ?>/how-to-use" class="text-sm">How To Use</a></li> -->
                 </ul>
             </nav>
         </div>
@@ -66,17 +74,6 @@
     <?= $this->renderSection('content') ?>
 
     <div class="flex-1"></div>
-
-    <!-- <footer class="bg-gray-800 text-gray-300 p-4">
-        <div class="container mx-auto text-center">
-            <p>
-                PT. CONNEXIST INDONESIA
-            </p>
-            <p>
-                Jl. Kemang Selatan IX No.9A, Jakarta Selatan, Jakarta, 12730
-            </p>
-        </div>
-    </footer> -->
 
     <script src="https://cdn.jsdelivr.net/npm/alpinejs@2.8.2"></script>
 
